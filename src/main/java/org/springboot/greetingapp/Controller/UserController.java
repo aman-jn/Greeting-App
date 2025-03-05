b4cac2ef0ae8801e2c6895b0d084d40ce187b8a3
Aman Jain <aj868911@gmail.com>
2025-03-05 22:49:10 +0530

    Added Sending Mails Functionality

--- src/main/java/org/springboot/greetingapp/Controller/UserController.java
+++ src/main/java/org/springboot/greetingapp/Controller/UserController.java
@@ -4,6 +4,7 @@ package org.springboot.greetingapp.Controller;
 import org.springboot.greetingapp.Entities.Auth;
 import org.springboot.greetingapp.Model.AuthUserDTO;
 import org.springboot.greetingapp.Model.LoginUserDTO;
+import org.springboot.greetingapp.Model.MailDTO;
 import org.springboot.greetingapp.Services.AuthenticationService;
 import org.springboot.greetingapp.Services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,17 +17,31 @@ public class UserController {
     @Autowired
     EmailService emailService;
     AuthenticationService authenticationService;
+
     public UserController(EmailService emailService, AuthenticationService authenticationService) {
         this.emailService = emailService;
         this.authenticationService = authenticationService;
     }
+
     // UC9 - For Registration of a User
-    @PostMapping("/register") public String registerUser(@RequestBody AuthUserDTO user){
-return authenticationService.register(user);
+    @PostMapping("/register")
+    public String registerUser(@RequestBody AuthUserDTO user) {
+        return authenticationService.register(user);
     }
+
     //UC10 - For User Login
-    @PostMapping("/login") public String loginUser(@RequestBody LoginUserDTO user){
+    @PostMapping("/login")
+    public String loginUser(@RequestBody LoginUserDTO user) {
         return authenticationService.login(user);
     }
 
+    //UC11 - For Sending Mails to User
+    @PostMapping("/sendMail")
+    public String sendMail(@RequestBody MailDTO mail) {
+        emailService.sendEmail(mail.getTo(),mail.getSubject(),mail.getMessage());
+        return "Mail Sent Successfully";
+    }
 }
+
+
+
